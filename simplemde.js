61aaf26842b1752b69c9013f9258ef49a536edde
Billy Fernández <byfernanz@gmail.com>
2019-09-19 18:13:12 -0400

    cambiado zoom de codigo a zoom web

--- simplemde.js
+++ simplemde.js
@@ -9,6 +9,7 @@ var panelDerecho=false;
 var panelIzquierdo=true;
 var panel=true;
 var espera=false;
+var zoom="100";
 // Fin
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SimpleMDE = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 'use strict'
@@ -18503,18 +18504,41 @@ function errorHandler(evt) {
     input.addEventListener('change', handleFileSelect, false);
 
 function toggleReducir(){
-  var sizeactual = $('.CodeMirror-code').css('fontSize');
+  /*var sizeactual = $('.CodeMirror-code').css('fontSize');
   var size = str_replace(sizeactual, 'px', '');
   size=parseInt(size)-3;
-  $('.CodeMirror-code').css({'fontSize': size});
+  $('.CodeMirror-code').css({'fontSize': size});*/
+  
+  /*var sizeactual = $('.CodeMirror-gutters').css('width');
+  var size = str_replace(sizeactual, 'px', '');
+  size=parseInt(size)-5;
+  $('.CodeMirror-gutters').css({'width': size});*/	
+  zoom.replace("%",""); 
+  zoom = parseInt(zoom) - 10 +'%';
+  document.body.style.zoom = zoom;
+  
   simplemde.codemirror.refresh();
 }
 
 function toggleAumentar(){
-  var sizeactual = $('.CodeMirror-code').css('fontSize');
+  /*var sizeactual = $('.CodeMirror-line').css('fontSize');
   var size = str_replace(sizeactual, 'px', '');
   size=parseInt(size)+3;
-  $('.CodeMirror-code').css({'fontSize': size});
+  $('.CodeMirror-code').css({'fontSize': size});*/
+  
+  /*var sizeactual =  $('.CodeMirror-sizer').css('margin-left');
+  var size = str_replace(sizeactual, 'px', '');
+  size=parseInt(size)+30;
+  $('.CodeMirror-sizer').css({'margin-left': size});*/
+  
+  /*var sizeactual = $('.CodeMirror-gutters').css('width');
+  var size = str_replace(sizeactual, 'px', '');
+  size=parseInt(size)+5;
+  $('.CodeMirror-gutters').css({'width': size});*/
+	zoom.replace("%",""); 
+    zoom = parseInt(zoom) + 10 +'%';
+    document.body.style.zoom = zoom;
+  
   simplemde.codemirror.refresh();
 }
 
